fabcbe31df74fc494bb61fc81b065a752533f757
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-05-19 00:02:34 -0500

    Created the project
    created the project and the first question file

---
+++ src/Solutions.java
@@ -0,0 +1,13 @@
+package src;
+/**
+ * 
+ * This project is not intended to execute in its current scope
+ * however I will try my best to give proper context to the solution
+ * by including the full code and partial question
+ * 
+ */
+
+public class Solutions {
+    public static void main(String[] args){
+    }
+}
