a1b7450a35260981f5803f8c6947133a0d306333
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-05-20 00:25:56 -0500

    added comments
    added comments that give context to the problem

--- src/SparseArray.java
+++ src/SparseArray.java
@@ -2,14 +2,19 @@
  * There is a collection of input strings and a collection of query strings.
  * For each query string, determine how many times it occurs in the list of input strings.
  * Return an array of the results.
+ * 
+ * EXAMPLE
+ * strings = [ab, ab, abc]
+ * queries = [ab, abc, bc]
+ * There are two instances of 'ab', one of 'abc' and zero of 'bc'.
+ * For each query, add an element to the return array, results=[2,1,0]
 */
+
 
 package src;
 
 import java.io.*;
 import java.util.*;
-
-
 
 public class SparseArray {
     
