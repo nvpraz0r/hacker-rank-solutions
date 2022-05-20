e6dc2174b56b8546769dc3c8b9fbea46138bd6a0
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-05-20 00:53:44 -0500

    created LonelyInteger class
    added comments to give context to the problem

---
+++ src/LonelyInteger.java
@@ -0,0 +1,25 @@
+/**
+ * Given an array of integers, where all elements but one occur twice, find the unique element.
+ * 
+ * SAMPLE INPUT
+ * 1 1 2
+ * 
+ * SAMPLE OUTPUT
+ * 2
+ * 
+ * EXAMPLE
+ * a=[1,2,3,4,1,2,3]
+ * the unique element is 4
+*/
+
+package src;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+
+public class LonelyInteger {
+    
+}
