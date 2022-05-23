cf20f62146358d4797ac4bd0229d5efec3b41156
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-05-23 18:03:30 -0500

    created Pangram class
    and added comments to give context to the challenge

---
+++ src/Pangram.java
@@ -0,0 +1,19 @@
+/**
+ * A pangram is a string that contains every letter of the alphabet. 
+ * Given a sentence determine whether it is a pangram in the English alphabet.
+ * Ignore case. Return either pangram or not pangram as appropriate.
+ * 
+ * SAMPLE INPUT
+ *  we promptly judged antique ivory buckles for the next prize
+ * 
+ * SAMPLE OUTPUT
+ * pangram
+*/
+
+package src;
+
+import java.io.*;
+
+public class Pangram {
+    
+}
